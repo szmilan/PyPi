200cfca1698918e629d1b71fba855dfe09fe0a94
szmilan <szimandl.milan@gmail.com>
2017-08-12 20:53:14 +0200

    kk

--- BLDC02.py
+++ BLDC02.py
@@ -3,26 +3,26 @@ import pigpio
 from read_RPM import reader
 import RPi.GPIO as GPIO
 import QuadNumeric
-import sys
-import termios
-import contextlib
 
 # Set up BCM GPIO numbering
 GPIO.setmode(GPIO.BCM)
 
 # Connect to pigpio
-pi = pigpio.pi() 
+pi = pigpio.pi()
+pi.start()
 
 # Calibrate ESC
 ESC_GPIO = 13
 
+
+
 # Set up input pins for thumbwheel switch
 try:
     while 1:
-        speed = 1.5		
-	key = sys.stdin.read(1)
-	if key == 72:
-		pi.set_servo_pulsewidth(ESC_GPIO, speed * 1000 / 7 + 1000)        
+        speed = 1.5			
+	
+	pi.set_servo_pulsewidth(ESC_GPIO, speed * 1000 / 7 + 1000)
+	
         sleep(100)
 
 finally:
